232c5fa220b61cf39a7e490159ba3091d9be7e37
Henri <h.allg2@gmx.de>
2023-06-29 20:12:31 +0200

    create simple kafka producer

--- code/anonymized_kafka/src/main/java/com/pipeline/kafka/DummyKafkaProducer.java
+++ code/anonymized_kafka/src/main/java/com/pipeline/kafka/DummyKafkaProducer.java
@@ -1,7 +1,11 @@
 package com.pipeline.kafka;
 
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
 
 public class DummyKafkaProducer {
     private static final Logger log = LoggerFactory.getLogger(DummyKafkaProducer.class);
@@ -9,5 +13,45 @@ public class DummyKafkaProducer {
     public static void main(String[] args) {
         log.info("Hello, world!");
         log.info("I am a Kafka Producer!");
+
+        String bootStrapServer = "localhost:9092";
+        String topic = "no-filter";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        int numberOfRecords = 25;
+
+        for (int i = 0; i < numberOfRecords; i++) {
+
+            String recordValue = "This is Mambo No. " + (i + 1);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, recordValue);
+
+            producer.send(producerRecord, (recordMetadata, e) -> {
+                if (e == null) {
+                    log.info("Received new metadata: \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                } else {
+                    log.error("Error while producing: ", e);
+                }
+            });
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        producer.flush();
+
+        producer.close();
     }
 }
