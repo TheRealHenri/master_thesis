d08f6ebbec60f31f6b2c14621ca05928c4d0954b
Henri <h.allg2@gmx.de>
2023-07-19 22:39:15 +0200

    implement zookeeper executor with monitor

--- code/anonymized_kafka/src/main/java/com/pipeline/kafka/zookeeper/DataMonitor.java
+++ code/anonymized_kafka/src/main/java/com/pipeline/kafka/zookeeper/DataMonitor.java
@@ -1,2 +1,112 @@
-package com.pipeline.kafka.zookeeper;public class DataMonitor {
+package com.pipeline.kafka.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Arrays;
+
+public class DataMonitor {
+
+    ZooKeeper zooKeeper;
+
+    String znode;
+
+    Watcher chainedWatcher;
+
+    boolean dead;
+
+    DataMonitorListener listener;
+
+    byte prevData[];
+
+    public DataMonitor(ZooKeeper zooKeeper, String znode, Watcher chainedWatcher, DataMonitorListener listener) {
+        this.zooKeeper = zooKeeper;
+        this.znode = znode;
+        this.chainedWatcher = chainedWatcher;
+        this.listener = listener;
+        zooKeeper.exists(znode, true, null, null);
+    }
+
+    public interface DataMonitorListener {
+        /**
+         * The existence status of the node has changed.
+         */
+        void exists(byte data[]);
+
+        /**
+         * The ZooKeeper session is no longer valid.
+         *
+         * @param rc
+         * the ZooKeeper reason code
+         */
+        void closing(KeeperException.Code rc);
+    }
+
+    public void processResult(KeeperException.Code rc, String path, Object ctx, Stat stat) {
+        boolean exists;
+        switch (rc) {
+            case OK:
+                exists = true;
+                break;
+            case NONODE:
+                exists = false;
+                break;
+            case SESSIONEXPIRED:
+                case NOAUTH:
+                dead = true;
+                listener.closing(rc);
+                return;
+            default:
+                // retry
+                zooKeeper.exists(znode, true, null, null);
+                return;
+        }
+        byte[] b = null;
+        if (exists) {
+            try {
+                b = zooKeeper.getData(znode, false, null);
+            } catch (KeeperException e) {
+                // Watcher handles this
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                return;
+            }
+        }
+        if ((b == null && b != prevData) || !Arrays.equals(prevData, b)) {
+            listener.exists(b);
+            prevData = b;
+        }
+    }
+
+
+    public void process(WatchedEvent event) {
+        String path = event.getPath();
+        if (event.getType() == Watcher.Event.EventType.None) {
+            // state of connection has changed
+            switch (event.getState()) {
+                case SyncConnected:
+                    // In this particular example we don't need to do anything
+                    // here - watches are automatically re-registered with
+                    // server and any watches triggered while the client was
+                    // disconnected will be delivered (in order of course)
+                    break;
+                case Expired:
+                    // Finito
+                    dead = true;
+                    listener.closing(KeeperException.Code.SESSIONEXPIRED);
+                    break;
+            }
+        } else {
+            if (path != null && path.equals(znode)) {
+                // something has changed on the node, let's find out
+                zooKeeper.exists(znode, true, null, null);
+            }
+        }
+        if (chainedWatcher != null) {
+            chainedWatcher.process(event);
+        }
+    }
 }
--- code/anonymized_kafka/src/main/java/com/pipeline/kafka/zookeeper/Executor.java
+++ code/anonymized_kafka/src/main/java/com/pipeline/kafka/zookeeper/Executor.java
@@ -1,2 +1,133 @@
-package com.pipeline.kafka.zookeeper;public class Executor {
+package com.pipeline.kafka.zookeeper;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.*;
+
+public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener {
+
+    String filename;
+
+    DataMonitor dataMonitor;
+
+    ZooKeeper zooKeeper;
+
+    String[] executable;
+
+    Process child;
+
+
+    public Executor(String hostPort, String znode, String filename, String[] executable) throws IOException {
+        this.filename = filename;
+        this.executable = executable;
+        this.zooKeeper = new ZooKeeper(hostPort, 3000, this);
+        this.dataMonitor = new DataMonitor(zooKeeper, znode, null, this);
+    }
+
+    public static void main(String[] args) {
+        if (args.length < 4) {
+            System.err.println("Usage: Executor <host> <port> <filename> <program> with [args ...]");
+            System.exit(2);
+        }
+        String hostPort = args[0];
+        String znode = args[1];
+        String filename = args[2];
+        String[] executable = new String [args.length - 3];
+        System.arraycopy(args, 3, executable, 0, executable.length);
+        try {
+            new Executor(hostPort, znode, filename, executable).run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            synchronized (this) {
+                while (!dataMonitor.dead) {
+                    wait();
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+        dataMonitor.process(watchedEvent);
+    }
+
+    @Override
+    public void exists(byte[] data) {
+        if (data == null) {
+            System.out.println("Killing process");
+            child.destroy();
+            try {
+                child.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            child = null;
+        } else {
+            if (child != null) {
+                System.out.println("Stopping child");
+                child.destroy();
+                try {
+                    child.waitFor();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                FileOutputStream fos = new FileOutputStream(filename);
+                fos.write(data);
+                fos.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                System.out.println("Starting child");
+                child = Runtime.getRuntime().exec(executable);
+                new StreamWriter(child.getInputStream(), System.out);
+                new StreamWriter(child.getErrorStream(), System.err);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void closing(KeeperException.Code rc) {
+        synchronized (this) {
+            notifyAll();
+        }
+    }
+
+    static class StreamWriter extends Thread {
+        OutputStream os;
+        InputStream is;
+
+        StreamWriter(InputStream is, OutputStream os) {
+            this.is = is;
+            this.os = os;
+            start();
+        }
+
+        public void run() {
+            byte[] b = new byte[80];
+            int rc;
+            try {
+                while ((rc = is.read(b)) > 0) {
+                    os.write(b,0,rc);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
