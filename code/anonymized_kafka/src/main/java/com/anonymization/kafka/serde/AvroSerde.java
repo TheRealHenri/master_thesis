54f953ff96b930771a70016480625f0f12e96602
Henri <h.allg2@gmx.de>
2023-09-07 11:32:12 +0200

    add avro serde logic

--- code/anonymized_kafka/src/main/java/com/anonymization/kafka/serde/AvroSerde.java
+++ code/anonymized_kafka/src/main/java/com/anonymization/kafka/serde/AvroSerde.java
@@ -1,6 +1,7 @@
 package com.anonymization.kafka.serde;
 
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.GenericRecord;
@@ -9,11 +10,14 @@ import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serializer;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 public class AvroSerde implements Serde<Struct>, Deserializer<Struct>, Serializer<Struct> {
@@ -59,12 +63,74 @@ public class AvroSerde implements Serde<Struct>, Deserializer<Struct>, Serialize
     }
 
     private GenericRecord structToGenericRecord(Struct struct) {
-        return null;
+        GenericRecord resultRecord = new GenericData.Record(schema);
+
+        for (Field field : struct.schema().fields()) {
+            resultRecord.put(field.name(), struct.get(field));
+        }
+
+        return resultRecord;
     }
 
     private Struct genericRecordToStruct(GenericRecord avroRecord) {
-        return null;
+        org.apache.kafka.connect.data.Schema kafkaSchema = convertAvroToKafkaSchema(schema);
+        Struct resultStruct = new Struct(kafkaSchema);
+
+        for (Field field : kafkaSchema.fields()) {
+            Object value = avroRecord.get(field.name());
+            resultStruct.put(field, value);
+        }
+
+        return resultStruct;
     }
+
+    public static org.apache.kafka.connect.data.Schema convertAvroToKafkaSchema(Schema avroSchema) {
+        SchemaBuilder kafkaSchemaBuilder = SchemaBuilder.struct().name(avroSchema.getName());
+        List<Schema.Field> avroFields = avroSchema.getFields();
+
+        for (Schema.Field avroField : avroFields) {
+            Schema.Type avroType = avroField.schema().getType();
+            String fieldName = avroField.name();
+
+            switch (avroType) {
+                case BOOLEAN:
+                    org.apache.kafka.connect.data.Schema typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_BOOLEAN_SCHEMA : org.apache.kafka.connect.data.Schema.BOOLEAN_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+                case INT:
+                    typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_INT32_SCHEMA : org.apache.kafka.connect.data.Schema.INT32_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+                case LONG:
+                    typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_INT64_SCHEMA : org.apache.kafka.connect.data.Schema.INT64_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+                case FLOAT:
+                    typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_FLOAT32_SCHEMA : org.apache.kafka.connect.data.Schema.FLOAT32_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+                case DOUBLE:
+                    typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_FLOAT64_SCHEMA : org.apache.kafka.connect.data.Schema.FLOAT64_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+                case STRING:
+                    typeSchema = avroField.schema().isNullable() ?
+                            org.apache.kafka.connect.data.Schema.OPTIONAL_STRING_SCHEMA : org.apache.kafka.connect.data.Schema.STRING_SCHEMA;
+                    kafkaSchemaBuilder.field(fieldName, typeSchema);
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("Unsupported Avro type: " + avroType);
+            }
+        }
+        return kafkaSchemaBuilder.build();
+    }
+
 
     @Override
     public Serializer<Struct> serializer() {
